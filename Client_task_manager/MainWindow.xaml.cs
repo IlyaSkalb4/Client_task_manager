c69a072047b543d13c3af732222fa362c34ba1d8
UserIllia <UserIllia@LAPTOP-IIAOG99R>
2023-10-20 21:05:08 +0200

    Add project files.

---
+++ Client_task_manager/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client_task_manager
+{
+    public partial class MainWindow : Window
+    {
+        private List<MyTask> tasks = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            tasks = new List<MyTask>
+            {
+                new MyTask { TaskTitle = "Task1", TaskText = "Do this and that", DeadlineValue = 33 },
+                new MyTask { TaskTitle = "Task2", TaskText = "Do that and this", DeadlineValue = 67 },
+                new MyTask { TaskTitle = "Task3", TaskText = "Do this and that", DeadlineValue = 14 },
+            };
+
+            foreach (MyTask task in tasks)
+            {
+                taskListBox.Items.Add(task);
+            }
+        }
+
+        private void CompleteMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (taskListBox.SelectedItems.Count < 1)
+            {
+                return;
+            }
+
+            taskListBox.Items.RemoveAt(taskListBox.SelectedIndex);
+        }
+    }
+
+    public class MyTask
+    {
+        public string TaskTitle { get; set; }
+
+        public string TaskText { get; set; }
+
+        public int DeadlineValue { get; set; }
+
+        public override string ToString()
+        {
+            return TaskTitle;
+        }
+    }
+}
---
+++ Client_task_manager/SignInWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Client_task_manager
+{
+    public partial class SignInWindow : Window
+    {
+        public SignInWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void logInButton_Click(object sender, RoutedEventArgs e)
+        {
+            Hide();
+
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.ShowDialog();
+
+            Close();
+        }
+
+        private void signUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            Hide();
+
+            SignUpWindow signUpWindow = new SignUpWindow();
+            signUpWindow.ShowDialog();
+
+            Show();
+        }
+    }
+}
